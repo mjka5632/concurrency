87fa8cd882577ebab769247ebea3ee4db601cfa9
Mart <mjka5632@126.com>
2018-03-27 21:43:10 +0800

    add annotation

---
+++ src/main/java/com/mmall/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,16 @@
+package com.mmall.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *线程【安全】的类或写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+}
